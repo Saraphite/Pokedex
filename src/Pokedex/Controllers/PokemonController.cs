4fc5616d5b058beccb60f2856506746017a97f7f
James Geater <saraphite@hotmail.co.uk>
2022-02-06 20:10:15 +0000

    Add PokemonController.

---
+++ src/Pokedex/Controllers/PokemonController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pokedex.Abstractions;
+using System.Threading.Tasks;
+
+namespace Pokedex.Controllers
+{
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly ILogger<PokemonController> _logger;
+
+        public PokemonController(ILogger<PokemonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[controller]/{name}")]
+        public async Task<Pokemon> GetAsync([FromRoute] string name)
+        {
+            var pokemon = new Pokemon
+            {
+                Name = name
+            };
+
+            return await Task.FromResult(pokemon);
+        }
+
+        [HttpGet]
+        [Route("[controller]/translated/{name}")]
+        public async Task<Pokemon> GetWithTranslationAsync([FromRoute] string name)
+        {
+            var pokemon = new Pokemon
+            {
+                Name = name
+            };
+
+            return await Task.FromResult(pokemon);
+        }
+    }
+}
