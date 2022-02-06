b5b215a6a3e89e31c71b680ff621f3d2b2da9dbb
James Geater <saraphite@hotmail.co.uk>
2022-02-06 17:22:15 +0000

    Add basic Pokedex Abstractions.

---
+++ src/Pokedex.Abstractions/Habitat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokedex.Abstractions
+{
+    public enum Habitat
+    {
+        Cave = 0,
+        Forest = 1,
+        Grassland = 2,
+        Mountain = 3,
+        Rare = 4,
+        RoughTerrain = 5,
+        Sea = 6,
+        Urban = 7,
+        WatersEdge = 8
+    }
+}
---
+++ src/Pokedex.Abstractions/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pokedex.Abstractions
+{
+    public class Pokemon
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Habitat Habitat { get; set; }
+        public bool IsLegendary { get; set; }
+    }
+}
